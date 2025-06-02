0d44c8a021c5765decc898adf35d6b7b0012ae28
Noah Sedlik <noahsedlik@berkeley.edu>
2025-06-02 01:07:19 -0700

    update example top module

--- sources/ztop.v
+++ sources/ztop.v
@@ -1,11 +1,14 @@
 // Basic Top Module for my
 // Basys 3 Board, ARTIX-7 FPGA Module
 module ztop(
-	input	clk, 
-	input sw, 
-	output reg led);
-	
-	always @(posedge clk)
-		led = sw;
+	input	     clk, 
+	input      [15:0] sw,
+	output reg [15:0] led
+);
+
+	for (genvar i = 0; i < 16; i = i + 1) begin
+		always @(posedge clk)
+			led[i] = sw[i];
+	end
 
 endmodule
